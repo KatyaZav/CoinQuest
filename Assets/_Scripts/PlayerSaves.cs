4d7fbea86b3a388bc03689fcf7f1060476bf0b94
Katya Zavyalova <82714610+KatyaZav@users.noreply.github.com>
2024-08-26 20:55:42 +0500

    add save logic

--- Assets/_Scripts/PlayerSaves.cs
+++ Assets/_Scripts/PlayerSaves.cs
@@ -1,40 +1,47 @@
+using YG;
+
 public static class PlayerSaves
 {
-    public static int CoinsInBank;
-    public static int CoinsInPocket;
+    public static int CoinsInBank => YandexGame.savesData.CoinsInBank;
+    public static int CoinsInPocket => YandexGame.savesData.CoinsInPocket;
 
     public static void PutCoinsToBank()
     {
-        CoinsInBank += CoinsInPocket;
-        CoinsInPocket = 0;
+        YandexGame.savesData.CoinsInBank += CoinsInPocket;
+        YandexGame.savesData.CoinsInPocket = 0;
 
         SubscriptionKeeper.ChangeBank();
         SubscriptionKeeper.ChangeMoneyValue();
+
+        YandexGame.SaveProgress();
     }
 
     public static void AddCoins(int value)
     {
-        CoinsInPocket += value;
+        YandexGame.savesData.CoinsInPocket += value;
 
         SubscriptionKeeper.ChangeMoneyValue();
+        YandexGame.SaveProgress();
     }
 
     public static void RemoveCoins(int value)
     {
-        CoinsInPocket -= value;
+        YandexGame.savesData.CoinsInPocket -= value;
 
         if (CoinsInPocket < 0)
         {
-            CoinsInPocket = 0;
+            YandexGame.savesData.CoinsInPocket = 0;
         }
 
         SubscriptionKeeper.ChangeMoneyValue();
+        YandexGame.SaveProgress();
     }
 
     public static void LooseCoins()
     {
-        CoinsInPocket = 0;
+        YandexGame.savesData.CoinsInPocket = 0;
 
         SubscriptionKeeper.ChangeMoneyValue();
+        YandexGame.SaveProgress();
     }
 }
