770ad4302ca5d33be6ea91c0f14ee17667a9b28c
Katya Zavyalova <82714610+KatyaZav@users.noreply.github.com>
2024-10-29 09:12:00 +0500

    save

---
+++ Assets/_Scripts/UIManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void LoadScene(int a)
+    {
+        SceneManager.LoadScene(a);
+    }
+
+
+}
