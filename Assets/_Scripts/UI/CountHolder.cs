3b87a2502ab1ac9d2e4c45603fe69d088bfbdb0f
Baronne_Noire <party2015ru@mail.ru>
2024-12-19 15:16:59 +0500

    add holder

--- Assets/_Scripts/UI/CountHolder.cs
+++ Assets/_Scripts/UI/CountHolder.cs
@@ -1,11 +1,35 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CountHolder : MonoBehaviour
 {
+    private const string Path = "Achives";
+
+    [SerializeField] private PopupUI _popupUI;
+
+    List<ScoreThreshold> _allScores;
+    List<ScoreThreshold> _scores;
+
     public void Init()
     {
         PlayerSaves.ChangedFinalBankMoney += OnChange;
+
+        var prev = PlayerSaves.PreviousMoney;
+        var cur = PlayerSaves.CoinsInBank;
+
+        _allScores = new ResourceLoader().Load<ScoreThreshold>(Path);
+        _allScores.Sort((a, b) => a.MinScore.CompareTo(b.MinScore));
+
+        _scores = new List<ScoreThreshold>(_allScores);
+
+        foreach (var score in _scores)
+        {
+            if (score.MinScore < prev || score.MinScore < cur)            
+                _scores.Remove(score);            
+            else
+                break;
+        }
     }
 
     private void OnDestroy()
@@ -15,6 +39,13 @@ public class CountHolder : MonoBehaviour
 
     private void OnChange(int prev, int cur)
     {
+        ScoreThreshold first = _scores[0];
+
+        if (first.MinScore < cur && first.MinScore > prev)
+        {
+            _popupUI.Activate(first.GetText(YG.YandexGame.lang), first.MinScore);
+            _scores.Remove(first);
+        }
     }
 }
 
