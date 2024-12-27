15a13a398267265cbceb7334320bf439cb75e03e
Baronne_Noire <party2015ru@mail.ru>
2024-12-27 16:43:14 +0500

    add paws events

---
+++ Assets/_Scripts/Events/Info/AddPawsEvent.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Events
+{
+    public class AddPawsEvent : IEvent
+    {
+        private EventData _data;
+        private int _pawCoins;
+
+        public AddPawsEvent(EventData data, int pawCoins)
+        {
+            _data = data;
+            _pawCoins = pawCoins;
+        }
+
+        public EventData EventData => _data;
+
+        public void Enter()
+        {
+            PlayerSaves.AddCoins(_pawCoins);
+        }
+
+        public void Exit()
+        {
+        }
+    }
+}
--- Assets/_Scripts/Events/Settings/EventsFabric.cs
+++ Assets/_Scripts/Events/Settings/EventsFabric.cs
@@ -6,21 +6,39 @@ namespace Events
 {
     public class EventsFabric
     {
-        private const string ChangeProbabilityEventDataPath = "Events/1_CatsMoreVigilance";
-        private const string ChangeProbabilityEventData2Path = "Events/2_CatsMoreVigilance";
-        private const string ChangeProbabilityEventData3Path = "Events/3_CatsMoreVigilance";
-        private const string ChangeProbabilityEventData4Path = "Events/4_CatsMoreVigilance";
-        private const string ChangeProbabilityEventData5Path = "Events/5_CatsMoreVigilance";
+        #region ProbabilityPaths
+        private const string ChangeProbabilityEventDataPath = "Events/Probability/1_CatsMoreVigilance";
+        private const string ChangeProbabilityEventData2Path = "Events/Probability/2_CatsMoreVigilance";
+        private const string ChangeProbabilityEventData3Path = "Events/Probability/3_CatsMoreVigilance";
+        private const string ChangeProbabilityEventData4Path = "Events/Probability/4_CatsMoreVigilance";
+        private const string ChangeProbabilityEventData5Path = "Events/Probability/5_CatsMoreVigilance";
+        #endregion
+
+        #region AddPawPath
+        private const string AddPawPath1 = "Events/AddPaw/AddPaw 1";
+        private const string AddPawPath2 = "Events/AddPaw/AddPaw 2";
+        private const string AddPawPath3 = "Events/AddPaw/AddPaw 3";
+        private const string AddPawPath4 = "Events/AddPaw/AddPaw 4";
+        #endregion
 
         public List<IEvent> GetFullEventsList()
         {
             List<IEvent> events = new List<IEvent>();
 
+            #region Probability
             events.Add(new ChangeProbabilityEvent(GetCoin(), 20, LoadData(ChangeProbabilityEventDataPath)));
             events.Add(new ChangeProbabilityEvent(GetCoin(), -20, LoadData(ChangeProbabilityEventData2Path)));
             events.Add(new ChangeProbabilityEvent(GetCoin(), -50, LoadData(ChangeProbabilityEventData3Path)));
             events.Add(new ChangeProbabilityEvent(GetCoin(), 50, LoadData(ChangeProbabilityEventData4Path)));
             events.Add(new ChangeProbabilityEvent(GetCoin(), -80, LoadData(ChangeProbabilityEventData5Path)));
+            #endregion
+
+            #region AddPaw
+            events.Add(new AddPawsEvent(LoadData(AddPawPath1), 2));
+            events.Add(new AddPawsEvent(LoadData(AddPawPath2), 3));
+            events.Add(new AddPawsEvent(LoadData(AddPawPath3), 5));
+            events.Add(new AddPawsEvent(LoadData(AddPawPath4), 1));
+            #endregion
 
             return events;
         }
