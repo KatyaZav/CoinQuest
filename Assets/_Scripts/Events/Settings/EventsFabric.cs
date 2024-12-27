89c3d0c918866b3c80103892e832c80b118a6eac
Baronne_Noire <party2015ru@mail.ru>
2024-12-27 16:11:26 +0500

    add new events

--- Assets/_Scripts/Events/Settings/EventsFabric.cs
+++ Assets/_Scripts/Events/Settings/EventsFabric.cs
@@ -7,12 +7,20 @@ namespace Events
     public class EventsFabric
     {
         private const string ChangeProbabilityEventDataPath = "Events/1_CatsMoreVigilance";
+        private const string ChangeProbabilityEventData2Path = "Events/2_CatsMoreVigilance";
+        private const string ChangeProbabilityEventData3Path = "Events/3_CatsMoreVigilance";
+        private const string ChangeProbabilityEventData4Path = "Events/4_CatsMoreVigilance";
+        private const string ChangeProbabilityEventData5Path = "Events/5_CatsMoreVigilance";
 
         public List<IEvent> GetFullEventsList()
         {
             List<IEvent> events = new List<IEvent>();
 
             events.Add(new ChangeProbabilityEvent(GetCoin(), 20, LoadData(ChangeProbabilityEventDataPath)));
+            events.Add(new ChangeProbabilityEvent(GetCoin(), -20, LoadData(ChangeProbabilityEventData2Path)));
+            events.Add(new ChangeProbabilityEvent(GetCoin(), -50, LoadData(ChangeProbabilityEventData3Path)));
+            events.Add(new ChangeProbabilityEvent(GetCoin(), 50, LoadData(ChangeProbabilityEventData4Path)));
+            events.Add(new ChangeProbabilityEvent(GetCoin(), -80, LoadData(ChangeProbabilityEventData5Path)));
 
             return events;
         }
