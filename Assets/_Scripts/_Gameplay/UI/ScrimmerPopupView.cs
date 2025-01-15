cf3c96f40e5ef0ae924a16bfa4247d361c367e25
Baronne_Noire <party2015ru@mail.ru>
2025-01-15 16:51:52 +0500

    fix cat move in popup

--- Assets/_Scripts/_Gameplay/UI/ScrimmerPopupView.cs
+++ Assets/_Scripts/_Gameplay/UI/ScrimmerPopupView.cs
@@ -8,6 +8,9 @@ namespace Assets.Gameplay.UI
 {
     public class ScrimmerPopupView : MonoBehaviour
     {
+        private readonly Vector2 _start = new Vector2(0, 320);
+        private readonly Vector2 _finish = new Vector2(0, -450);
+
         private readonly Vector2 _minScale = Vector2.zero;
         private readonly Vector2 _maxScale = Vector2.one;
 
@@ -19,11 +22,15 @@ namespace Assets.Gameplay.UI
         [SerializeField] private CanvasGroup _canvasGroup;
         [SerializeField] private RectTransform _catTransform;
 
-        private AnimationTween _animation;
-        private Tween _tweenCat;
+        private AnimationTween _animation, _catAnimation;
 
         public void Init()
         {
+            var start = new MovingFactory(_catTransform, _finish, _start, Duration);
+            var finish = new MovingFactory(_catTransform, _start, _finish, Duration);
+
+            _catAnimation = new AnimationTween(start, finish);
+
             var startAnimation = new PopupAnimationFactory
                 (new PopupInfo(_canvasGroup, _popupZone), _minScale, _maxScale, MinFade, MaxFade, Duration);
 
@@ -40,24 +47,17 @@ namespace Assets.Gameplay.UI
 
         public void Activate(Action callback = null)
         {
-            _animation.Activate(callback);
-
-            _tweenCat?.Kill();
-            _tweenCat = _catTransform
-                .DOMoveY(320, Duration)
-                .From(-450)
-                .SetEase(Ease.InOutQuart);
+            _animation.Activate(() =>
+            {
+                _catAnimation.Activate();
+                callback?.Invoke();
+            });
         }
 
         public void Deactivate(Action callback = null)
         {
             _animation.Disactivate(callback);
-
-            _tweenCat?.Kill();
-            _tweenCat = _catTransform
-                .DOMoveY(-450, Duration)
-                .From(320)
-                .SetEase(Ease.InOutQuart);
+            _catAnimation.Disactivate();
         }
     }
 }
